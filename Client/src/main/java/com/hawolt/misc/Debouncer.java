e181aab276c6e070bced154fa26f2a445ebe49e9
hawolt <45050975+hawolt@users.noreply.github.com>
2024-01-05 18:43:25 +0100

    add debouncer

---
+++ Client/src/main/java/com/hawolt/misc/Debouncer.java
@@ -0,0 +1,25 @@
+package com.hawolt.misc;
+
+import java.util.concurrent.*;
+
+public class Debouncer {
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private final ConcurrentHashMap<Object, Future<?>> delayedMap = new ConcurrentHashMap<>();
+
+    public void debounce(final Object key, final Runnable runnable, long delay, TimeUnit unit) {
+        final Future<?> prev = delayedMap.put(key, scheduler.schedule(() -> {
+            try {
+                runnable.run();
+            } finally {
+                delayedMap.remove(key);
+            }
+        }, delay, unit));
+        if (prev != null) {
+            prev.cancel(true);
+        }
+    }
+
+    public void shutdown() {
+        scheduler.shutdownNow();
+    }
+}
