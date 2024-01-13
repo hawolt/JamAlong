9c5477027e8e19bb95d819ca470e9267581d5bcc
hawolt <45050975+hawolt@users.noreply.github.com>
2024-01-13 19:28:42 +0100

    fix remote client uri

--- Client/src/main/java/com/hawolt/remote/RemoteClient.java
+++ Client/src/main/java/com/hawolt/remote/RemoteClient.java
@@ -16,7 +16,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 public class RemoteClient extends WebSocketClient {
-    private static String host = "ws://127.0.0.1:48157/?sudo=false&name=%s";
+    private static String host = "ws://hawolt.com:48157/?sudo=false&name=%s";
 
     public static RemoteClient createAndConnectClientInstance(String username) {
         try {
